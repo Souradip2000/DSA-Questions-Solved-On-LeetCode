2ffee9fe317409f2672457adc1893430a683d13f
Souradip Ghosh <84944565+Souradip2000@users.noreply.github.com>
2023-11-01 22:51:03 +0530

    Time: 168 ms (96.34%), Space: 61.3 MB (98.06%) - LeetHub

--- 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.cpp
+++ 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.cpp
@@ -8,12 +8,16 @@ public:
             reverse(temp.begin(), temp.end());
             final.push_back(stoi(temp));
         }
-        // add all elements to a set
-        set<int> st;
-        for(int i: final){
-            st.insert(i);
-        }
-        // return the size of the set
-        return st.size();
+        sort(final.begin(), final.end());
+        auto it=unique(final.begin(), final.end());
+        final.erase(it, final.end());
+        return final.size();
+        // // add all elements to a set
+        // set<int> st;
+        // for(int i: final){
+        //     st.insert(i);
+        // }
+        // // return the size of the set
+        // return st.size();
     }
 };
