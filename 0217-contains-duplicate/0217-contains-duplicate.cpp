76410c0fbcbef57b4eea5980c0a4f8b65851b3a9
Souradip Ghosh <84944565+Souradip2000@users.noreply.github.com>
2023-11-14 18:34:31 +0530

    Time: 168 ms (5.09%), Space: 73.8 MB (6.03%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int> st;
+        for(int i: nums){
+            st.insert(i);
+        }
+        if(st.size()==nums.size())  return false;
+        return true;
+    }
+};
